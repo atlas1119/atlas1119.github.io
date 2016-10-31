0e0db5690f6c3e7d736c2f0425706f09ed16fb76
wangshaojun <wangshaojun@zrcaifu.com>
2016-10-31 15:31:44 +0800

    增加联系方式和增加markdown语法

--- javascripts/components/Contact.js
+++ javascripts/components/Contact.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 export default class Contact extends React.Component{
-
+    constructor(props, context){
+        super(props, context);
+        this.state={
+            expanded:true,
+            expanded1:true,
+            expanded2:true
+        };
+    }
     componentDidMount(){
-
+        
     }
 
     render(){
         return (
             <div className="nav-top">
-                手机号：16800466042
+                <Card expanded={this.state.expanded} onExpandChange={(expanded)=>{this.setState({expanded});}}>
+                    <CardHeader
+                      title="联系信息"
+                      subtitle="QQ，Email，手机号等"
+                      avatar="/dist/images/avatar.jpeg"
+                      actAsExpander={true}
+                      showExpandableButton={true}
+                    />
+
+                    <CardText expandable={true}>
+                        Email：<a href="mailto:wsj.atlas1119@gmail.com">wsj.atlas1119@gmail.com</a><br/>
+                        QQ：604984034@qq.com<br/>
+                        手机号：18600466042
+                    </CardText>
+                </Card>
             </div>
         );
     }
--- javascripts/components/Tech.js
+++ javascripts/components/Tech.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import showdown from 'showdown';
 
 export default class Tech extends React.Component{
+    constructor(props, context){
+        super(props, context);
 
+        this.state={
+            html:''
+        };
+
+    }
     componentDidMount(){
+        var converter = new showdown.Converter(),
+            text      = '#hello, markdown!',
+            html      = converter.makeHtml(text);
         
+        this.setState({html});
     }
 
     render(){
         return (
             <div className="nav-top">
-                这块会不断更新，正在想办法，如何自动更新。。。
+                <div dangerouslySetInnerHTML={{__html: this.state.html}}></div>
             </div>
         );
     }
