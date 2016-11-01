3f5960d084f0ad330c7f326f9b953e879832ad6e
wangshaojun <wangshaojun@zrcaifu.com>
2016-11-01 16:49:03 +0800

    增加loading

--- javascripts/components/Detail.js
+++ javascripts/components/Detail.js
@@ -8,16 +8,16 @@ export default class Tech extends React.Component{
     constructor(props, context){
         super(props, context);
         this.state = {
-            html:'<p style="text-align:center;font-size:28px;padding-top:120px;">加载中...</p>'
+            html:'  <div class="loader"><div class="dot"></div><div class="dot"></div><div class="dot"></div><div class="dot"></div><div class="dot"></div></div>'
         };
 
     }
     componentDidMount(){
         var id = this.props.params.id;
-        // axios.get("html/"+ id +".md").then((res)=>{
-        //     var html = converter.makeHtml(res.data);
-        //     this.setState({html});
-        // });
+        axios.get("html/"+ id +".md").then((res)=>{
+            var html = converter.makeHtml(res.data);
+            this.setState({html});
+        });
 
     }
 
